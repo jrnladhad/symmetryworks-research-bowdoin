d592b245d814a56d2c55d52d7514aaeb0b42bff4
Jay Ladhad <jayladhad06@gmail.com>
2018-07-20 12:13:55 -0700

    Update functions.h

--- Wallpaper Generation/functions.h
+++ Wallpaper Generation/functions.h
@@ -212,7 +212,7 @@ public:
 #define Xrect ( (2*pi*x) / Krect)
 #define Yrect ( (2*pi*y) / Lrect)
 
-///////////////////////////
+////////////////////////////////////////////////////////////////
 class pmFunction : public AbstractFunction
 {
 public:
@@ -225,7 +225,7 @@ public:
     
     virtual AbstractFunction* clone() const { return new pmFunction(*this); };
 };
-///////////////////////////
+////////////////////////////////////////////////////////////////
 class pmmFunction : public AbstractFunction
 {
 public:
@@ -239,7 +239,7 @@ public:
     virtual AbstractFunction* clone() const { return new pmmFunction(*this); };
 };
 
-///////////////////////////
+////////////////////////////////////////////////////////////////
 class pggFunction : public AbstractFunction
 {
 public:
@@ -252,7 +252,7 @@ public:
     
     virtual AbstractFunction* clone() const { return new pggFunction(*this); };
 };
-///////////////////////////
+////////////////////////////////////////////////////////////////
 class pmgFunction : public AbstractFunction
 {
 public:
@@ -265,7 +265,7 @@ public:
     
     virtual AbstractFunction* clone() const { return new pmgFunction(*this); };
 };
-///////////////////////////
+////////////////////////////////////////////////////////////////
 class pgFunction : public AbstractFunction
 {
 public:
@@ -365,7 +365,7 @@ public:
     
     
 };
-/////////////////////////////////
+
 ////////////////////////////////////////////////////////////////
 class p4mFunction : public AbstractFunction
 {
@@ -416,212 +416,4 @@ public:
 
 ///////////////////////////////////////////////////////////////
 
-//This locks z^n\bar{z}^m with reciprocal for inversion symmetry
-
-class invFunction : public AbstractFunction
-{
-  public:
-    invFunction() { terms = 1; refresh(); }
-    invFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    invFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{ return new invFunction(*this);};
-
-};
-
-///////////////////////////////////////////////////////////////
-
-//This locks z^n\bar{z}^m with minus reciprocal for negative inversion symmetry
-
-class neginvFunction : public AbstractFunction
-{
-  public:
-    neginvFunction() { terms = 1; refresh(); }
-    neginvFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    neginvFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new neginvFunction(*this);};
-
-};
-
-///////////////////////////////////////////////////////////////
-
-//Tetrahedral symmetry
-
-class tetraFunction : public AbstractFunction
-{
-  public:
-    tetraFunction() { terms = 1; refresh(); }
-    tetraFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    tetraFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new tetraFunction(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Tetrahedral symmetry with 3 poles and 3 zeroes.
-
-class tetra3Function : public AbstractFunction
-{
-  public:
-    tetra3Function() { terms = 1; refresh(); }
-    tetra3Function(unsigned int in_terms) { terms = in_terms; refresh(); }
-    tetra3Function(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new tetra3Function(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Tetrahedral color-turning symmetry with 3 poles and 3 zeroes
-
-class tetraColFunction : public AbstractFunction
-{
-  public:
-    tetraColFunction() { terms = 1; refresh(); }
-    tetraColFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    tetraColFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new tetraColFunction(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Tetrahedral symmetry with the mirror that locks (N,M) with (M,N)
-
-class tetraMFunction : public AbstractFunction
-{
-  public:
-    tetraMFunction() { terms = 1; refresh(); }
-    tetraMFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    tetraMFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new tetraMFunction(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Icosahedral symetry
-
-class icosFunction : public AbstractFunction
-{
-  public:
-    icosFunction() { terms = 1; refresh(); }
-    icosFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    icosFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new icosFunction(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Icosahedral symmetry starting with 3 poles and 3 zeroes
-
-class icos3Function : public AbstractFunction
-{
-  public:
-    icos3Function() { terms = 1; refresh(); }
-    icos3Function(unsigned int in_terms) { terms = in_terms; refresh(); }
-    icos3Function(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new icos3Function(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Icosahedral symmetry done with invariant rational functions
-
-class icos5Function : public AbstractFunction
-{
-  public:
-    icos5Function() { terms = 1; refresh(); }
-    icos5Function(unsigned int in_terms) { terms = in_terms; refresh(); }
-    icos5Function(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new icos5Function(*this);};
-
-};
-
-///////////////////////////////////////////////////////////////
-
-//Icosahedral symmetry done with invariant rational functions
-
-class icos30Function : public AbstractFunction
-{
-  public:
-    icos30Function() { terms = 1; refresh(); }
-    icos30Function(unsigned int in_terms) { terms = in_terms; refresh(); }
-    icos30Function(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new icos30Function(*this);};
-};
-
-///////////////////////////////////////////////////////////////
-
-//Tetrahedral symmetry done with spherical harmonics
-
-class tetraHFunction : public AbstractFunction
-{
-  public:
-    tetraHFunction() { terms = 1; refresh(); }
-    tetraHFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    tetraHFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new tetraHFunction(*this);};
-
-};
-
-///////////////////////////////////////////////////////////////
-
-//Icosahedral symmetry done with sperical harmonics
-
-class icosHFunction : public AbstractFunction
-{
-  public:
-    icosHFunction() { terms = 1; refresh(); }
-    icosHFunction(unsigned int in_terms) { terms = in_terms; refresh(); }
-    icosHFunction(QVector<coeffpair> in_coeffs, QVector<freqpair> in_freqs) {initWithVectors(in_coeffs, in_freqs);}
-
-    std::complex<double> bundle(double &x, double &y, unsigned int &i) const;
-    std::complex<double> operator() (double i, double j);
-
-    virtual AbstractFunction* clone() const{return new icosHFunction(*this);};
-
-};
-
-///////////////////////////////////////////////////////////////
 #endif // FUNCTIONS_H
